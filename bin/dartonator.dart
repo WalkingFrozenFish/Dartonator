7bbb033150d1ba20c38c1492693b4559ebee3fef
Oleg Chachin <chachin.ol@yandex.kz>
2024-09-08 13:21:21 +0500

    Initial

---
+++ bin/dartonator.dart
@@ -0,0 +1,9 @@
+import 'package:dartonator/dartonator.dart' as dartonator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartonator.calculate()}!');
+
+  var someVar = "123";
+
+  print(someVar);
+}
---
+++ test/dartonator_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartonator/dartonator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
